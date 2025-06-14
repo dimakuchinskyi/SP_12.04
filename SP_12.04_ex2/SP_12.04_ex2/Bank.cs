baac8eef7fd3c0947d9c56c040a3d7970f93fd53
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2025-06-14 14:32:25 +0300

    Add files via upload

---
+++ SP_12.04_ex2/SP_12.04_ex2/Bank.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+public class Bank
+{
+    private int _money;
+    private string _name;
+    private int _percent;
+    private readonly object _lock = new object();
+
+    public int Money
+    {
+        get => _money;
+        set
+        {
+            _money = value;
+            StartLoggingThread();
+        }
+    }
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+            StartLoggingThread();
+        }
+    }
+
+    public int Percent
+    {
+        get => _percent;
+        set
+        {
+            _percent = value;
+            StartLoggingThread();
+        }
+    }
+
+    private void StartLoggingThread()
+    {
+        Thread thread = new Thread(LogToFile);
+        thread.Start();
+    }
+
+    private void LogToFile()
+    {
+        lock (_lock)
+        {
+            string log = $"Name: {_name}, Money: {_money}, Percent: {_percent}, Time: {DateTime.Now}\n";
+            File.AppendAllText("bank_log.txt", log);
+        }
+    }
+}
---
+++ SP_12.04_ex2/SP_12.04_ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Bank bank = new Bank();
+        bank.Name = "MyBank";
+        bank.Money = 1000;
+        bank.Percent = 5;
+
+        Console.WriteLine("Bank properties updated. Check bank_log.txt for logs.");
+    }
+}
