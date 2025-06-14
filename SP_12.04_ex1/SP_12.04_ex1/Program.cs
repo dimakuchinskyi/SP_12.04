87426670f56384ba5a981c6f04f204443346d861
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2025-06-14 14:31:39 +0300

    Add files via upload

---
+++ SP_12.04_ex1/SP_12.04_ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        List<object> items = new List<object> { 1, "hello", 3.14, DateTime.Now };
+
+        Thread thread = new Thread(() => PrintCollection(items));
+        thread.Start();
+        thread.Join();
+    }
+
+    static void PrintCollection(IEnumerable<object> collection)
+    {
+        foreach (var item in collection)
+        {
+            Console.WriteLine(item.ToString());
+        }
+    }
+}
