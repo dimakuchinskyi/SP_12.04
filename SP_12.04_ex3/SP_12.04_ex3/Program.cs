0625f657d8f4d07106cfb1e88045fa08988b6d63
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2025-06-14 14:32:48 +0300

    Add files via upload

---
+++ SP_12.04_ex3/SP_12.04_ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Reaction Time Game: Press any key as soon as you see 'GO!'");
+        Random rnd = new Random();  
+        
+        int waitTime = rnd.Next(2000, 5000);
+        Thread.Sleep(waitTime);
+
+        Console.WriteLine("GO!");
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        Console.ReadKey(true);
+
+        stopwatch.Stop();
+        Console.WriteLine($"Your reaction time: {stopwatch.ElapsedMilliseconds} ms");
+    }
+}
